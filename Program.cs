09ecc635f4c55e172760776471bcf761ffdc2639
Martin Zarutski <martinzar5@gmail.com>
2023-06-26 15:25:17 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+List<string> name = new List<string>() { "Marta", "Bob", "James", "Adam", "Sam", "Paul"};
+
+foreach (string nameItem in name)
+{
+    Console.WriteLine(nameItem);
+}
+
+name.AddRange(new[] { "Artem", "Sony" });
+
+Console.WriteLine(Environment.NewLine);
+
+foreach (string nameItem in name)
+{
+    Console.WriteLine($"{nameItem}");
+}
+
+name.RemoveAt(2);
+
+Console.WriteLine(Environment.NewLine);
+
+foreach(string nameItem in name)
+{
+    Console.WriteLine(nameItem);
+}
+
+name.Remove("Sony");
+
+Console.WriteLine(Environment.NewLine);
+
+foreach(string nameItem in name)
+{
+    Console.WriteLine(nameItem);
+}
+
+name.Sort();
+
+Console.WriteLine(Environment.NewLine);
+
+foreach (string nameItem in name)
+{
+    Console.WriteLine(nameItem);
+}
